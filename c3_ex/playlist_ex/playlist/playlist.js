f0bf8522c8887fadc0fd51d379c77a9ac63ebad1
SeoeunPark <s2019w29@e-mirim.hs.kr>
2021-03-11 16:04:52 +0900

    playlist

--- c3_ex/playlist_ex/playlist/playlist.js
+++ c3_ex/playlist_ex/playlist/playlist.js
@@ -1,14 +1,30 @@
 /* playlist.js */
 
 window.onload = init;
-
+//페이지가 완전히 로드된 후 호출되어 실행, 실행되지 않도록 init함수를 사용함
 function init() {
 	var button = document.getElementById("addButton");
+	//“노래추가” 버튼에 접근하기
 	button.onclick = handleButtonClick;
-
+	//버튼에 클릭 핸들러 부여하기
 	loadPlaylist();
 }
-
-	//추가1
-
-
+function handleButtonClick(e) {
+	var textInput = document.getElementById("songTextInput");
+	var songName = textInput.value;
+	//alert("Adding " + songName);
+	if (songName == "") {
+		alert("곡을 입력하세요.");
+	}
+	else {
+		//alert("Adding " + songName);
+		var li = document.createElement("li");
+		//노래제목에 들어갈 새 <li>요소를 만듭니다.
+		li.innerHTML = songName;
+		var ul = document.getElementById("playlist");
+		//id가 playlist인 ul이 새로 만든 <li>의 부모 요소입니다. 
+		ul.appendChild(li);
+		// for Ready Bake
+		save(songName);
+	}
+}
